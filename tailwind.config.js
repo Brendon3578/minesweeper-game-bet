f64ca13305e12c20e072cfa697fed71155a69079
Brendon Gomes <brendongomes40@gmail.com>
2024-04-16 23:10:43 -0300

    :rocket: first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ED1836",
+        secondary: "#FAF755",
+        background: "#0F1923",
+        "true-black": "#090f15",
+        "true-gray": "#d4d4d4",
+      },
+    },
+  },
+  plugins: [],
+};
